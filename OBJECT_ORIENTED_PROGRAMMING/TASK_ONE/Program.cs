ac8fa658978cc136ddca36541871f18049462fa3
Hakkı <hakki.ceylan2356@gmail.com>
2022-03-19 13:14:55 +0300

    added object oriented programming task one

---
+++ OBJECT_ORIENTED_PROGRAMMING/TASK_ONE/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+/****************************************************************************
+**					SAKARYA ÜNİVERSİTESİ
+**				BİLGİSAYAR VE BİLİŞİM BİLİMLERİ FAKÜLTESİ
+**				    BİLGİSAYAR MÜHENDİSLİĞİ BÖLÜMÜ
+**				   NESNEYE DAYALI PROGRAMLAMA DERSİ
+**					2021-2022 BAHAR DÖNEMİ
+**	
+**				ÖDEV NUMARASI..........: 1
+**				ÖĞRENCİ ADI............: Hakkı Ceylan
+**				ÖĞRENCİ NUMARASI.......: G211210350
+**                         DERSİN ALINDIĞI GRUP...: 2. Öğretim - A Grubu
+****************************************************************************/
+
+namespace TaskOne
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Sifrenizi Giriniz: ");
+            string Sifre = "";
+            ConsoleKeyInfo OkunanKarakter;
+            do
+            {
+                OkunanKarakter = Console.ReadKey();
+                switch ((char)OkunanKarakter.KeyChar)
+                {
+                    case '\b':
+                        Sifre = Sifre.Substring(0, Sifre.Length - 1);
+                        Console.Write(" \b");
+                        break;
+                    default:
+                        Sifre += OkunanKarakter.KeyChar;
+                        break;
+                }
+                GirdiyiDegerlendir(Sifre);
+            } while (OkunanKarakter.KeyChar != '\r');
+            Console.Write("\n\n\n\n\n");
+            SifreDogrulayici.SifreDogrula(Sifre);
+        }
+
+        private static void GirdiyiDegerlendir(string sifre)
+        {
+            (int, int) ConsoleIsaretci = Console.GetCursorPosition();
+            Console.WriteLine("\n");
+            Tuple<string, int> DegerlendirmeSonucu = SifreDogrulayici.SifreTestEt(sifre);
+            Console.Write("[");
+            int Ilerleme = (DegerlendirmeSonucu.Item2 <= 100 ? DegerlendirmeSonucu.Item2 : 100) / 5;
+            for (int i = 0; i < Ilerleme; i++) Console.Write("*");
+            for (int i = 0; i < 20 - Ilerleme; i++) Console.Write(" ");
+            Console.Write("] %"+ (Ilerleme * 5) + "\n");
+            Console.Write(DegerlendirmeSonucu.Item1);
+            for (int i = 0; i < 50 - DegerlendirmeSonucu.Item1.Length; i++) Console.Write(" ");
+            Console.SetCursorPosition(ConsoleIsaretci.Item1, ConsoleIsaretci.Item2);
+        }
+
+    }
+}
---
+++ OBJECT_ORIENTED_PROGRAMMING/TASK_ONE/SifreDogrulayici.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+/****************************************************************************
+**					SAKARYA ÜNİVERSİTESİ
+**				BİLGİSAYAR VE BİLİŞİM BİLİMLERİ FAKÜLTESİ
+**				    BİLGİSAYAR MÜHENDİSLİĞİ BÖLÜMÜ
+**				   NESNEYE DAYALI PROGRAMLAMA DERSİ
+**					2021-2022 BAHAR DÖNEMİ
+**	
+**				ÖDEV NUMARASI..........: 1
+**				ÖĞRENCİ ADI............: Hakkı Ceylan
+**				ÖĞRENCİ NUMARASI.......: G211210350
+**                         DERSİN ALINDIĞI GRUP...: 2. Öğretim - A Grubu
+****************************************************************************/
+
+namespace TaskOne
+{
+    static class SifreDogrulayici
+    {
+        public static Regex BUYUK_HARF_DOGRULAMA = new(@"[A-ZÇŞĞÜİÖ]");
+        public static Regex KUCUK_HARF_DOGRULAMA = new(@"[a-zçşğüıö]");
+        public static Regex RAKAM_DOGRULAMA = new(@"[0-9]");
+        public static Regex SEMBOL_DOGRULAMA = new(@"[\+\-\*\/\!\'\^\%\&\/\/\(\)\=\\\.\#\$\{\[\]\}\|\,]");
+
+        public static int BuyukHarfSayisi(string Sifre)
+        {
+            return BUYUK_HARF_DOGRULAMA.Matches(Sifre).Count;
+        }
+
+        public static int KucukHarfSayisi(string Sifre)
+        {
+            return KUCUK_HARF_DOGRULAMA.Matches(Sifre).Count;
+        }
+        public static int RakamSayisi(string Sifre)
+        {
+            return RAKAM_DOGRULAMA.Matches(Sifre).Count;
+        }
+
+        public static int SembolSayisi(string Sifre)
+        {
+            return SEMBOL_DOGRULAMA.Matches(Sifre).Count;
+        }
+
+        public static Tuple<string, int> SifreTestEt(string Sifre)
+        {
+
+            int Puan = 0;
+
+            int KucukHarfSayisi = SifreDogrulayici.KucukHarfSayisi(Sifre);
+            int BuyukHarfSayici = SifreDogrulayici.BuyukHarfSayisi(Sifre);
+            int SembolSayisi = SifreDogrulayici.SembolSayisi(Sifre);
+            int RakamSayisi = SifreDogrulayici.RakamSayisi(Sifre);
+            if (Sifre.Count() > 9)
+            {
+                if (KucukHarfSayisi == 0)
+                {
+                    return new Tuple<string, int>("Sifreniz kucuk harf icermelidir", 0);
+                }
+                if (BuyukHarfSayici == 0)
+                {
+                    return new Tuple<string, int>("Sifreniz buyuk harf icermelidir", 0);
+                }
+                if (SembolSayisi == 0)
+                {
+                    return new Tuple<string, int>("Sifreniz sembol icermelidir\n Uygun Karakterler: +, -, *, /, !, ', ^, %, &, /, /, (, ), =,\\, ., #, $, {, [, ], }, |, ','", 0);
+                }
+                if (RakamSayisi == 0)
+                {
+                    return new Tuple<string, int>("Sifreniz rakam icermelidir", 0);
+                }
+            }
+            Puan += KucukHarfSayisi > 2 ? 20 : KucukHarfSayisi * 10;
+            Puan += BuyukHarfSayici > 2 ? 20 : BuyukHarfSayici * 10;
+            Puan += RakamSayisi > 2 ? 20 : RakamSayisi * 10;
+            Puan += SembolSayisi * 10;
+            Puan += Sifre.Count() == 9 ? 10 : 0;
+            if (Puan < 70)
+            {
+                return new Tuple<string, int>("Sifre yeterince guclu degil.", Puan);
+            }
+            if (Puan < 90)
+            {
+                return new Tuple<string, int>("Sifre yeterince guclu.", Puan);
+            }
+            return new Tuple<string, int>("Sifre cok guclu!", Puan);
+
+        }
+
+        public static Tuple<string, int> SifreDogrula(string Sifre)
+        {
+
+            int Puan = 0;
+            if (Sifre.Count() < 9)
+            {
+                return new Tuple<string, int>("Sifre 9 karakterden daha uzun olmalidir.", 0);
+            }
+
+            int KucukHarfSayisi = SifreDogrulayici.KucukHarfSayisi(Sifre);
+            int BuyukHarfSayici = SifreDogrulayici.BuyukHarfSayisi(Sifre);
+            int SembolSayisi = SifreDogrulayici.SembolSayisi(Sifre);
+            int RakamSayisi = SifreDogrulayici.RakamSayisi(Sifre);
+            if (Sifre.Count() > 9)
+            {
+                if (KucukHarfSayisi == 0)
+                {
+                    return new Tuple<string, int>("Sifreniz kucuk harf icermelidir", 0);
+                }
+                if (BuyukHarfSayici == 0)
+                {
+                    return new Tuple<string, int>("Sifreniz buyuk harf icermelidir", 0);
+                }
+                if (SembolSayisi == 0)
+                {
+                    return new Tuple<string, int>("Sifreniz sembol icermelidir", 0);
+                }
+                if (RakamSayisi == 0)
+                {
+                    return new Tuple<string, int>("Sifreniz rakam icermelidir", 0);
+                }
+            }
+            Puan += KucukHarfSayisi > 2 ? 20 : KucukHarfSayisi * 10;
+            Puan += BuyukHarfSayici > 2 ? 20 : BuyukHarfSayici * 10;
+            Puan += RakamSayisi > 2 ? 20 : RakamSayisi * 10;
+            Puan += SembolSayisi * 10;
+            Puan += Sifre.Count() == 9 ? 10 : 0;
+            Console.WriteLine("KucukHarfSayisi: " + KucukHarfSayisi);
+            Console.WriteLine("BuyukHarfSayisi: " + BuyukHarfSayici);
+            Console.WriteLine("RakamHarfSayisi: " + RakamSayisi);
+            Console.WriteLine("SembolHarfSayisi: " + SembolSayisi);
+            if (Puan < 70)
+            {
+                return new Tuple<string, int>("Sifre yeterince guclu degil.", Puan);
+            }
+            if (Puan < 90)
+            {
+                return new Tuple<string, int>("Sifre yeterince guclu.", Puan);
+            }
+            return new Tuple<string, int>("Sifre cok guclu!", Puan);
+
+        }
+    }
+}
